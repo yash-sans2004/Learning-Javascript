9fbd7c5ae3e5bde0f890fe8451aa0f051bd77a46
Yash Purwar <nayan251198@gmail.com>
2023-02-23 23:05:50 +0530

    Methods of arrays

--- String.js
+++ String.js
@@ -1,18 +1,18 @@
-let name = "Yash";
-console.log(name);
-console.log(name.length);
+// let name = "Yash";
+// console.log(name);
+// console.log(name.length);
 
-// Template Literals
-let boy1 = "Yash"
-let girl1 = "Sanskriti"
-let sentence = `${boy1} is a boyfriend of ${girl1}`;
-console.log(sentence)
+// // Template Literals
+// let boy1 = "Yash"
+// let girl1 = "Sanskriti"
+// let sentence = `${boy1} is a boyfriend of ${girl1}`;
+// console.log(sentence)
 
-// String Properties
-let name = "Yash Purwar"
-let name1 = "Sanskriti"
-let newname = name.concat(name1)
-console.log(newname)
+// // String Properties
+// let name = "Yash Purwar"
+// let name1 = "Sanskriti"
+// let newname = name.concat(name1)
+// console.log(newname)
 
 // Practice
 let name = "YashPurwar"
