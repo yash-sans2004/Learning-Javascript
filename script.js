22eaa7eaf9247ab34b33e5cf9aeead9cc5b4ae69
Yash Purwar <nayan251198@gmail.com>
2023-03-05 12:02:45 +0530

    Use a alert, prompt, confirm function

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("Geeks for Geeks");
+console.warn("Do not drink soda")
+alert("Hello your script works") // alert function
+let a = prompt("Yes! My script works") //  prompt function
+let write = confirm("Write your name")
+if(write){
+    document.write(a);
+}
+
+else{
+    console.log("Better luck next time")
+}
